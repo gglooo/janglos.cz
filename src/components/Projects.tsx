cd162668fe59c1e014cac6f6d4025a30aff6d890
Jan Glos <xglos@fi.muni.cz>
2023-06-05 13:04:34 +0200

    feat: add portfolio project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,7 @@ import Project from "./Project";
 import pv178 from "../assets/pv178.png";
 import brainbyte from "../assets/brainbyte.png";
 import karlos from "../assets/karlos.png";
+import win95 from "../assets/win95.png";
 
 export const Projects = () => {
     const projects = [
@@ -35,6 +36,13 @@ export const Projects = () => {
                 "One of my first projects – I created a website for a customer, which displays models of steel structures. It was mainly focused on the ease of adding new content to the website which I achieved by dynamically generating the content from a filesystem. I also created a script in Python which translated important IFC data into a json file.",
             image: karlos,
             onClick: () => window.open("http://www.ok.9e.cz/"),
+        },
+        {
+            title: "Windws95 themed portfolio",
+            stack: ["TypeScript", "React", "TailwindCSS", "Git", "Vite"],
+            description:
+                "The portfolio you are viewing right now is my most recent project. I put emphasis on retro visuals which take you back to the 90s. The main focus was to make the website as intuitive as possible - eg. when you see a bin icon, you can throw stuff into it, then take it out etc. Every window is draggable and resizable, each icon can be dragged and dropped anywhere on the desktop. Real Brno weather data is fetched from an API and displayed in my Weather widget. The website is responsive and should work on mobile devices as well.",
+            image: win95,
         },
     ];
 
