670c2b9520f6b3b21563dc474bbeb1accfc973ec
Jan Glos <jan.glos@inqool.cz>
2023-10-08 23:49:10 +0200

    chore: update texts

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 export const About = () => {
     const technologies = [
         "React",
+        "React Native",
         "TypeScript",
         "Prisma",
         "Git",
@@ -18,8 +19,9 @@ export const About = () => {
             <section>
                 <h1 className="text-3xl font-bold">Jan Glos</h1>
                 <p className="text-dark-grey text-md">
-                    Programming and Development student | C seminar tutor &&
-                    code reviewer | Python code reviewer | Web developer
+                    Programming and Development student | Web developer | Mobile
+                    developer | C seminar tutor && code reviewer | Python code
+                    reviewer
                 </p>
                 <div className="flex flex-row flex-wrap py-4">
                     {technologies.map((technology, index) => (
@@ -38,25 +40,25 @@ export const About = () => {
                     <p className="text-lg">
                         Hi! My name is Jan and I am a student of Programming and
                         Development at the Faculty of Informatics at Masaryk
-                        University in Brno. I have recently finished my second
-                        year of studies. I am really passionate about
+                        University in Brno. I have recently started doing the
+                        third year of my studies. I am really passionate about
                         programming, problem solving and new technologies in
                         general.
                     </p>
                     <br />
                     <p className="text-lg">
-                        Web development is my favorite activity right now,
-                        however, I never stick to one field as I try to be
+                        Web/mobile development is my favorite activity right
+                        now, however, I never stick to one field as I try to be
                         versatile and adaptable to new opportunities. I have
                         built applications in C# or C and wrote a few scripts in
                         Python and bash.
                     </p>
                     <br />
                     <p className="text-lg">
-                        I am currently looking for an (ideally) full-time
-                        internship to further improve my skills, gain experience
-                        in the field and learn from experienced developers. If
-                        you have any questions do not hesitate to{" "}
+                        I am not currently actively looking for a job offer.
+                        However, if you think your proposal might sound
+                        interesting to me or if you have any questions do not
+                        hesitate to{" "}
                         <a
                             href="https://www.linkedin.com/in/jan-glos-21007b202/"
                             className="underline"
