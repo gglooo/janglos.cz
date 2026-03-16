67265def093af6904253653ebc4fefb46fe63273
Jan Glos <jan.glos@inqool.cz>
2026-03-16 13:11:47 +0100

    fix: move mirror higher

--- src/components/black-lodge/mirror/mirrorConfig.ts
+++ src/components/black-lodge/mirror/mirrorConfig.ts
@@ -2,7 +2,7 @@ export const BLACK_LODGE_LEFT_WALL_X = -12.86;
 
 export const BLACK_LODGE_MIRROR_POSITION: [number, number, number] = [
     BLACK_LODGE_LEFT_WALL_X + 0.04,
-    2.2,
+    2.4,
     0,
 ];
 
